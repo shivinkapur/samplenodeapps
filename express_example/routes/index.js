b2180b52a9008b5606997601e94e43ed76bc913a
shivinkapur <shivinkapur@hotmail.com>
2014-10-01 15:13:19 +0530

    complete sample code

---
+++ express_example/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+module.exports = router;
